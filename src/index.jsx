d9a162e5566fada36a3e8be51f1a5ef01d3f723e
dkegle <domenkeglevic@gmail.com>
2018-09-25 13:32:24 +0200

    Webpack setup

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from "react";
+
+
+ReactDOM.render(<h1>Hello config!</h1>, document.getElementById('react-app'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const IgnoreAssetsWebpackPlugin = require('ignore-assets-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader', 
+                options: {presets: ["@babel/react"]}}
+            ]
+        }
+    ]
+   },
+   plugins:[ // inline scripts into index.html and avoid writing index.js to disk
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inlineSource: '.(js)$'
+        }),
+        new HtmlWebpackInlineSourcePlugin(),
+        new IgnoreAssetsWebpackPlugin({
+            ignore: 'index.js'
+        })
+   ]
+};
